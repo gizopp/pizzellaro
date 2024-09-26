2172bc1eb472ce13381e54d4a4daf8429f4a83e7
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-09-25 23:53:36 -0300

    feat: page style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,33 @@ import Link from "next/link";
 
 export default async function Page() {
   return (
-    <div className={styles.containerCenter}>
-      <Image src={logoImg} alt="Logo" />
+    <>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo" />
 
-      <section className={styles.login}>
-        <form>
-          <input
-            type="email"
-            required
-            name="email"
-            placeholder="joao@pizzellaro.com"
-            className={styles.input}
-          />
-          <input
-            type="password"
-            required
-            name="password"
-            placeholder="********"
-            className={styles.input}
-          />
-          <button type="submit">Acessar</button>
-        </form>
-        <Link href="/signup" className={styles.text}>
-          Não possui uma conta? Cadastre-se
-        </Link>
-      </section>
-    </div>
+        <section className={styles.login}>
+          <form>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="joao@pizzellaro.com"
+              className={styles.input}
+            />
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="********"
+              className={styles.input}
+            />
+            <button type="submit">Acessar</button>
+          </form>
+          <Link href="/signup" className={styles.text}>
+            Não possui uma conta? Cadastre-se
+          </Link>
+        </section>
+      </div>
+    </>
   );
 }
