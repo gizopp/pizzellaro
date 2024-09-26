cac0505c5bab1a82f1e796c9e70d5ca63313a387
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-09-26 00:16:18 -0300

    feat: signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,45 @@
+import styles from "../page.module.scss";
+import Image from "next/image";
+import logoImg from "/public/logo.svg";
+import Link from "next/link";
+
 export default function Signup() {
   return (
     <>
-      <main>
-        <h1>Cadastro de Usuário</h1>
-      </main>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo" />
+
+        <section className={styles.login}>
+          <h1>Criando sua conta</h1>
+          <form>
+            <input
+              type="text"
+              required
+              name="name"
+              placeholder="João Pizzellaro"
+              className={styles.input}
+            />
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="joao@pizzellaro.com"
+              className={styles.input}
+            />
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="********"
+              className={styles.input}
+            />
+            <button type="submit">Cadastrar</button>
+          </form>
+          <Link href="/" className={styles.text}>
+            Já possui uma conta? Faça login
+          </Link>
+        </section>
+      </div>
     </>
   );
 }
