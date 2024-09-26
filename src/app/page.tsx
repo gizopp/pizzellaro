14d5552f72f7aac02a7e2d2a9fd3699bece3bf65
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-09-26 12:52:31 -0300

    feat: user login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,61 @@
 import styles from "./page.module.scss";
 import Image from "next/image";
-import logoImg from "/public/logo.svg";
+import logoImg from "/public/logoPizzellaro.svg";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { api } from "@/services/app";
+import { cookies } from "next/headers";
 
 export default async function Page() {
+  async function handleLogin(formData: FormData) {
+    "use server";
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (!email || !password) {
+      console.log("Please fill in all fields");
+      return;
+    }
+
+    try {
+      const response = await api.post("/session", {
+        email,
+        password,
+      });
+
+      if (!response.data.token) {
+        console.log("Invalid credentials");
+        return;
+      }
+
+      cookies().set("session", response.data.token, {
+        maxAge: 60 * 60 * 24 * 30 * 1000,
+        path: "/",
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production",
+      });
+      console.log("User registered successfully!");
+    } catch (err) {
+      console.log("Error registering user: " + err);
+      return;
+    }
+
+    redirect("/dashboard");
+  }
+
   return (
     <>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo" />
 
         <section className={styles.login}>
-          <form>
+          <form action={handleLogin}>
             <input
               type="email"
               required
               name="email"
-              placeholder="joao@pizzellaro.com"
+              placeholder="john@pizzellaro.com"
               className={styles.input}
             />
             <input
@@ -25,10 +65,10 @@ export default async function Page() {
               placeholder="********"
               className={styles.input}
             />
-            <button type="submit">Acessar</button>
+            <button type="submit">Login</button>
           </form>
           <Link href="/signup" className={styles.text}>
-            Não possui uma conta? Cadastre-se
+            Don't have an account? Sign up
           </Link>
         </section>
       </div>
