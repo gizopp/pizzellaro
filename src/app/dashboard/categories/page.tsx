5f06ef4bf1349bba2ede8b44be2b0676c9ef7e4c
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-09-27 19:32:13 -0300

    feat: init with categories components

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,3 +1,19 @@
+import styles from "./page.module.scss";
+
 export default function Categories() {
-  return <></>;
+  return (
+    <main className={styles.container}>
+      <h1>New Category</h1>
+      <form className={styles.form}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Category Name, example: Pizzas"
+          required
+          className={styles.input}
+        />
+        <button>Register</button>
+      </form>
+    </main>
+  );
 }
