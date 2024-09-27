2b46ea948b7354049f212c23bfc93ddeac119d9c
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-09-27 00:41:38 -0300

    feat: delete session cookie on logout

--- src/app/dashboard/components/header/index.tsx
+++ src/app/dashboard/components/header/index.tsx
@@ -3,8 +3,15 @@ import Image from "next/image";
 import styles from "./styles.module.scss";
 import logoImg from "/public/logoPizzellaro.svg";
 import { LogOutIcon } from "lucide-react";
+import { deleteCookie } from "cookies-next";
+import router from "next/router";
 
 export default function Header() {
+  const handleLogout = () => {
+    deleteCookie("session", { path: "/" });
+    router.replace("/");
+  };
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
@@ -21,7 +28,7 @@ export default function Header() {
         <nav>
           <Link href="dashboard/categories">Categories</Link>
           <Link href="dashboard/products">Menu</Link>
-          <form action="">
+          <form action={handleLogout}>
             <button>
               <LogOutIcon size={24} color="#fff" />
             </button>
