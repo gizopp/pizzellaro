22f009444df46ac5901409f243a706e660df8ab4
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-09-27 19:54:25 -0300

    feat: new input component

---
+++ src/app/dashboard/components/button/index.tsx
@@ -0,0 +1,20 @@
+import React, { ButtonHTMLAttributes } from "react";
+import styles from "./styles.module.scss";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary";
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  variant = "primary",
+  ...props
+}) => {
+  return (
+    <button className={`${styles.button} ${styles[variant]}`} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
