964df33e6a720b275e6f01a333f93d2bd55a6ff2
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-09-29 22:36:02 -0300

    feat: clear input after register category

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useRef } from "react";
 import { Toaster, toast } from "react-hot-toast";
 import Button from "../components/button";
 import Input from "../components/input";
@@ -7,10 +7,13 @@ import styles from "./page.module.scss";
 import { CategoryCreate } from "@/services/category/CategoryCreate";
 
 export default function Categories() {
+  const categoryForm = useRef<HTMLFormElement>(null);
+
   const handleRegisterCategory = async (formData: FormData) => {
     try {
       await CategoryCreate(String(formData.get("name")));
       toast.success("Category registered successfully!");
+      categoryForm.current?.reset();
     } catch (error) {
       console.error("Error registering category:", error);
       toast.error("Failed to register category. Please try again.");
@@ -21,7 +24,11 @@ export default function Categories() {
     <main className={styles.container}>
       <Toaster position="top-center" />
       <h1>New Category</h1>
-      <form className={styles.form} action={handleRegisterCategory}>
+      <form
+        className={styles.form}
+        action={handleRegisterCategory}
+        ref={categoryForm}
+      >
         <Input
           type="text"
           name="name"
