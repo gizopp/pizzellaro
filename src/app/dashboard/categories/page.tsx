8c73d9ed69b84b9f98a4064a29105e05664966fa
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-09-29 22:25:13 -0300

    feat: category register

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,12 +1,22 @@
+"use client";
 import Button from "../components/button";
 import Input from "../components/input";
 import styles from "./page.module.scss";
+import { CategoryCreate } from "@/services/category/CategoryCreate";
 
 export default function Categories() {
+  const handleRegisterCategory = async (formData: FormData) => {
+    try {
+      const response = await CategoryCreate(String(formData.get("name")));
+    } catch (error) {
+      console.error("Error registering category:", error);
+    }
+  };
+
   return (
     <main className={styles.container}>
       <h1>New Category</h1>
-      <form className={styles.form}>
+      <form className={styles.form} action={handleRegisterCategory}>
         <Input
           type="text"
           name="name"
@@ -14,7 +24,7 @@ export default function Categories() {
           required
           style={{ marginBottom: "1rem" }}
         />
-        <Button>Register</Button>
+        <Button name="Register" type="submit" />
       </form>
     </main>
   );
