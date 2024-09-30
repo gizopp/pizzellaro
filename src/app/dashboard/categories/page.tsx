90127cf1f88e09b61cf397432061c5fec13d211b
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-09-29 22:33:02 -0300

    feat: improve toast on category create

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import { Toaster, toast } from "react-hot-toast";
 import Button from "../components/button";
 import Input from "../components/input";
 import styles from "./page.module.scss";
@@ -7,14 +9,17 @@ import { CategoryCreate } from "@/services/category/CategoryCreate";
 export default function Categories() {
   const handleRegisterCategory = async (formData: FormData) => {
     try {
-      const response = await CategoryCreate(String(formData.get("name")));
+      await CategoryCreate(String(formData.get("name")));
+      toast.success("Category registered successfully!");
     } catch (error) {
       console.error("Error registering category:", error);
+      toast.error("Failed to register category. Please try again.");
     }
   };
 
   return (
     <main className={styles.container}>
+      <Toaster position="top-center" />
       <h1>New Category</h1>
       <form className={styles.form} action={handleRegisterCategory}>
         <Input
