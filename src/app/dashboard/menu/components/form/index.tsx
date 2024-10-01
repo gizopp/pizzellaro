d6faca04a4ad42845b9704934d765539d1be59a8
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-10-01 00:26:35 -0300

    feat: product form

--- src/app/dashboard/menu/components/form/index.tsx
+++ src/app/dashboard/menu/components/form/index.tsx
@@ -3,8 +3,9 @@
 import styles from "./styles.module.scss";
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
-import Input from "@/app/components/input";
 import { InputImage } from "@/app/components/inputImage";
+import Input from "@/app/components/input";
+import InputSelect from "@/app/components/inputSelect";
 
 export function Form() {
   const [image, setImage] = useState<File>();
@@ -13,6 +14,11 @@ export function Form() {
     setImage(image);
   };
 
+  const options = [
+    { value: "1", label: "Pizzas" },
+    { value: "2", label: "Drinks" },
+  ];
+
   return (
     <main className={styles.container}>
       <Toaster />
@@ -20,12 +26,27 @@ export function Form() {
       <form className={styles.form} action="">
         <label className={styles.labelImage}>
           <InputImage onImageChange={handleImageChange} />
-          <Input
-            type="email"
-            required
-            name="email"
-            placeholder="john@pizzellaro.com"
-          />
+          <div className={styles.inputContainer}>
+            <InputSelect name="category" options={options} value={"category"} />
+            <Input
+              type="text"
+              name="name"
+              placeholder="Product name..."
+              required
+            />
+            <Input
+              type="text"
+              name="price"
+              placeholder="Product price..."
+              required
+            />
+            <Input
+              type="text"
+              name="description"
+              placeholder="Product description..."
+              required
+            />
+          </div>
         </label>
       </form>
     </main>
