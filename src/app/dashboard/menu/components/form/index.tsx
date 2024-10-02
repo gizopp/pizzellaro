7b2432583e1afc2b9200142a955c780117341f91
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-10-02 00:08:23 -0300

    feat: product register

--- src/app/dashboard/menu/components/form/index.tsx
+++ src/app/dashboard/menu/components/form/index.tsx
@@ -1,13 +1,17 @@
 "use client";
 import styles from "./styles.module.scss";
-import { useState } from "react";
-import { Toaster } from "react-hot-toast";
+import { useRef, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { InputImage } from "@/app/components/inputImage";
 import Input from "@/app/components/input";
 import InputSelect from "@/app/components/inputSelect";
 import InputTextArea from "@/app/components/inputTextArea";
 import Button from "@/app/components/button";
 import { Category } from "@/app/types/category";
+import { api } from "@/services/app";
+import { headers } from "next/headers";
+import { getCoookieClient } from "@/lib/cookieClient";
+import { redirect } from "next/navigation";
 
 interface FormProps {
   categories: Category[];
@@ -16,6 +20,35 @@ interface FormProps {
 export function Form({ categories }: FormProps) {
   const [image, setImage] = useState<File | null>(null);
   const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleRegisterProduct = async (formData: FormData) => {
+    const category = formData.get("category");
+    const name = formData.get("name");
+    const price = formData.get("price");
+    const description = formData.get("description");
+
+    if (!category || !name || !price || !description || !image) {
+      toast.error("All fields are required!");
+      return;
+    }
+
+    const productData = new FormData();
+
+    productData.append("category_id", category);
+    productData.append("name", name);
+    productData.append("price", price);
+    productData.append("description", description);
+    productData.append("file", image);
+    const token = getCoookieClient();
+
+    await api.post("/product", productData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    toast.success("Product registered successfully");
+  };
 
   const handleImageChange = (image: File | null) => {
     setImage(image);
@@ -36,7 +69,7 @@ export function Form({ categories }: FormProps) {
     <main className={styles.container}>
       <Toaster />
       <h1>New Product</h1>
-      <form className={styles.form} action="">
+      <form className={styles.form} action={handleRegisterProduct}>
         <label className={styles.labelImage}>
           <InputImage onImageChange={handleImageChange} />
           <div className={styles.inputContainer}>
