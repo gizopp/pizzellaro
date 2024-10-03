9145743d5bf757d0a3de41983201b14bad43b15d
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-10-03 00:02:44 -0300

    feat: ordersGet

---
+++ src/app/api/orders/OrdersGet.tsx
@@ -0,0 +1,17 @@
+import { Order } from "@/app/types/order.type";
+import { getCoookieServer } from "@/lib/cookieServer";
+import { api } from "@/services/app";
+
+export const OrdersGet = async (): Promise<Order[] | []> => {
+  try {
+    const token = getCoookieServer();
+    const response = await api.get("/orders", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return response.data || [];
+  } catch (error) {
+    console.error("Error getting orders: " + error);
+    return [];
+  }
+};
