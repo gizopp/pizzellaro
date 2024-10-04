1f81226242286c012ddce06d387e3d017a18b1f7
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-10-03 23:21:03 -0300

    feat: ordersGet on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,8 @@
+import { OrdersGet } from "../api/orders/OrdersGet";
 import { Orders } from "../components/orders";
 
-export default function Dashboard() {
-  return (
-    <>
-      <Orders />
-    </>
-  );
+export default async function Dashboard() {
+  const orders = await OrdersGet();
+
+  return <Orders orders={orders} />;
 }
