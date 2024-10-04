15862e20e2141c5b5c324c13d74860d72e673a89
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-10-04 00:25:49 -0300

    feat: improve OrderModal on dashboard

--- src/app/components/orders/index.tsx
+++ src/app/components/orders/index.tsx
@@ -1,5 +1,6 @@
 import { RefreshCw } from "lucide-react";
 import styles from "./styles.module.scss";
+import { OrderModal } from "../modal";
 
 interface Orders {
   orders: Order[];
@@ -7,21 +8,24 @@ interface Orders {
 
 export function Orders({ orders }: Orders) {
   return (
-    <main className={styles.container}>
-      <section className={styles.containerHeader}>
-        <h1>Last orders</h1>
-        <button>
-          <RefreshCw size={24} color="#3fffa3" />
-        </button>
-      </section>
-      <section className={styles.listOrders}>
-        {orders.map((order) => (
-          <button key={order.id} className={styles.orderItem}>
-            <div className={styles.tag}></div>
-            <span>Table {order.table}</span>
+    <>
+      <main className={styles.container}>
+        <section className={styles.containerHeader}>
+          <h1>Last orders</h1>
+          <button>
+            <RefreshCw size={24} color="#3fffa3" />
           </button>
-        ))}
-      </section>
-    </main>
+        </section>
+        <section className={styles.listOrders}>
+          {orders.map((order) => (
+            <button key={order.id} className={styles.orderItem}>
+              <div className={styles.tag}></div>
+              <span>Table {order.table}</span>
+            </button>
+          ))}
+        </section>
+      </main>
+      <OrderModal order={orders[0]} />
+    </>
   );
 }
