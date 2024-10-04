90916a109b7b0d716c95b195f4acf747c68c59e8
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-10-04 08:38:22 -0300

    feat: use OrderContext on order details modal

--- src/app/components/modal/index.tsx
+++ src/app/components/modal/index.tsx
@@ -2,16 +2,25 @@ import { X } from "lucide-react";
 import styles from "./styles.module.scss";
 import { Order } from "@/app/types/order.type";
 import Button from "../button";
+import { use } from "react";
+import { OrderContext } from "@/providers/order";
 
 interface ModalProps {
   order: Order;
 }
 
 export const OrderModal = ({ order }: ModalProps) => {
+  const { onRequestClose } = use(OrderContext);
+
   return (
     <dialog className={styles.dialogContainer}>
       <section className={styles.dialogContent}>
-        <button className={styles.dialogButton}>
+        <button
+          className={styles.dialogButton}
+          onClick={() => {
+            onRequestClose();
+          }}
+        >
           <X size={24} color="#fff" />
         </button>
         <article className={styles.detailsContainer}>
