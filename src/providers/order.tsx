2a933cda03c871bc87bec6b79c35cbad130dea08
˜gizopp˜ <giovanazoppelaro@gmail.com>
2024-10-08 15:31:45 -0300

    feat: improve order on OrderProvider

--- src/providers/order.tsx
+++ src/providers/order.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { OrderItem } from "@/app/types/orderItem.type";
+import { getCoookieClient } from "@/lib/cookieClient";
+import { api } from "@/services/app";
 import { createContext, useState } from "react";
 
 type OrderContextData = {
   isOpen: boolean;
-  onRequestOpen: (order_id: string) => void;
+  onRequestOpen: (order_id: string) => Promise<void>;
   onRequestClose: () => void;
+  order: OrderItem[];
 };
 
 type OrderProviderProps = {
@@ -16,8 +20,23 @@ export const OrderContext = createContext({} as OrderContextData);
 
 export function OrderProvider({ children }: OrderProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [order, setOrder] = useState<OrderItem[]>([]);
 
-  function onRequestOpen(order_id: string) {
+  async function onRequestOpen(order_id: string) {
+    const token = getCoookieClient();
+
+    const response = await api.get("/orderdetails", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        order_id,
+      },
+    });
+
+    console.log(response.data);
+
+    setOrder(response.data);
     setIsOpen(true);
   }
 
@@ -26,7 +45,9 @@ export function OrderProvider({ children }: OrderProviderProps) {
   }
 
   return (
-    <OrderContext.Provider value={{ isOpen, onRequestOpen, onRequestClose }}>
+    <OrderContext.Provider
+      value={{ isOpen, onRequestOpen, onRequestClose, order }}
+    >
       {children}
     </OrderContext.Provider>
   );
